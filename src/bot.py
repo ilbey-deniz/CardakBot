f987032926a434e18b43f8eb75b7c93f0f0c204c
e2448314 <e2448314@ceng.metu.edu.tr>
2024-01-24 17:07:03 +0300

    simple bot

---
+++ src/bot.py
@@ -0,0 +1,24 @@
+import dotenv
+import discord
+import os
+
+dotenv.load_dotenv()
+
+if __name__ == "__main__":
+    client = discord.Client(intents=discord.Intents.default())
+
+    @client.event
+    async def on_ready():
+        print("Logged in as")
+        print(client.user.name)
+        print(client.user.id)
+        print("------")
+
+    @client.event
+    async def on_message(message):
+        if message.content.startswith("!hello"):
+            await message.channel.send("Hello World!")
+        else:
+            print("Message received: " + message.content)
+
+    client.run(os.getenv("DISCORD_TOKEN"))
