7c9a60ff209352198e163dded6f13875a93c8988
e2448314 <e2448314@ceng.metu.edu.tr>
2024-01-24 16:00:46 +0300

    initial commit

---
+++ deneme.py
@@ -0,0 +1,9 @@
+class Deneme:
+    def __init__(self):
+        self.i = 0
+    
+    def incr(self):
+        self.i += 1
+
+    def getI(self):
+        return self.i
---
+++ test_deneme.py
@@ -0,0 +1,12 @@
+import unittest
+from deneme import Deneme
+
+class TestDeneme(unittest.TestCase):
+    def test_increment(self):
+       deneme = Deneme()
+       i = deneme.getI()
+       deneme.incr()
+       self.assertEqual(i + 1, deneme.getI())
+
+if __name__ == "__main__":
+    unittest.main()
