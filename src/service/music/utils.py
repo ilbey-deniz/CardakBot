657a6083ed56daf0b47f5f66aea58c92906caf39
ilbey-deniz <mustafa.ilbey@hotmail.com>
2024-02-14 17:25:10 +0300

    added spotify /track link support

--- src/service/music/utils.py
+++ src/service/music/utils.py
@@ -16,24 +16,25 @@ class YTMusicUtils:
     #if it is a spotify url, get the song name and search for it over ytmusic
     #if it is a search query, search for it over ytmusic
     def getSongByQuery(self, query) -> Song:
+        song = None
         if "https://www.youtube.com/watch?v=" in query:
             video_id = query.split("watch?v=")[1]
             song_info = self.ytmusic.get_song(video_id)
-            return Song(song_info["title"], song_info["artists"][0]["name"], video_id)
+            song = Song(song_info["title"], song_info["artists"][0]["name"], video_id)
 
-        elif "https://open.spotify.com/track/" in query:
+        elif "open.spotify.com/track/" in query:
             try:
-                track_id = query.split("track/")[1]
-                track = self.sp.track(track_id)
+                track = self.sp.track(query)
                 song_name = track["name"]
                 artist = track["artists"][0]["name"]
                 search_results = self.ytmusic.search(query=song_name + " " + artist, filter="songs")
                 video_id =  search_results[0]["videoId"]
-                return Song(song_name, artist, video_id)
+                song = Song(song_name, artist, video_id)
             except SpotifyException as e:
-                print(e)
+                print("spotipy exception: " + e)
             
         else:
             search_results = self.ytmusic.search(query=query, filter="songs")
-            return Song(search_results[0]["title"], search_results[0]["artists"][0]["name"], search_results[0]["videoId"])
-    
+            song = Song(search_results[0]["title"], search_results[0]["artists"][0]["name"], search_results[0]["videoId"])
+
+        return song
